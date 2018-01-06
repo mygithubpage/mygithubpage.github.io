c1b630eef30c59654819c4b95da0ee946349dac2
decisact@tutanota.com <decisact@yandex.com>
2018-01-06 22:39:53 +0800

    Update

--- external.js
+++ external.js
@@ -44,12 +44,13 @@ function sidebarFunc() {
 
 function accFunc(name) {
     var x = document.getElementById(name);
+    var color = document.getElementsByTagName('footer')[0].className.split(' ')[1];
     if (x.className.indexOf("w3-show") == -1) {
         x.className += " w3-show";
-        x.previousElementSibling.className += " w3-light-blue";
+        x.previousElementSibling.className += " " + color;
     } else { 
         x.className = x.className.replace(" w3-show", "");
         x.previousElementSibling.className = 
-        x.previousElementSibling.className.replace(" w3-light-blue", "");
+        x.previousElementSibling.className.replace(" " + color, "");
     }
 }
